e09319efbffc9fa63b99450dd09ca6868b07e247
Ximena <ximenaha00@gmail.com>
2023-11-09 14:53:50 +0800

    try to make it draggable but fail

--- my-editable-card-app/src/App.js
+++ my-editable-card-app/src/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import './App.css'; // Make sure to include the CSS from your previous HTML
+import './App.css';
 
 function App() {
   // Function to handle the end of a drag event
   const onDragEnd = (result) => {
-    // Logic to reorder the list and update the state goes here
+    // You can implement the logic to reorder the list and update the state here
+    // For now, we'll just log the result for demonstration purposes
+    console.log(result);
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="droppable">
         {(provided) => (
-          <div {...provided.droppableProps} ref={provided.innerRef}>
+          // Added className for styling droppable area
+          <div {...provided.droppableProps} ref={provided.innerRef} className="droppable-container">
             <Draggable key="1" draggableId="draggable-1" index={0}>
               {(provided) => (
-                <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  // Added inline style for absolute positioning within the droppable container
+                  style={{
+                    ...provided.draggableProps.style,
+                    position: 'absolute',
+                  }}
+                >
                   <div className="card">
                     <div contentEditable="true" className="editable-text"></div>
                   </div>
