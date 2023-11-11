5a0c8b47d2edb920de8f83352c411d31f5ee1b97
Ximena <ximenaha00@gmail.com>
2023-11-11 15:50:39 +0800

    almost done for executing the code

---
+++ function/password-tab/pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../app/globals.css'; // Adjust the path if your globals.css is located elsewhere
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
